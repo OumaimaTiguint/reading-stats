664409fe817b762fd99636a8fdf563d46e3c1738
OumaimaTiguint <oumaimatiguint@gmail.com>
2020-11-02 14:42:03 +0100

    added bubble map

---
+++ src/components/map-values.js
@@ -0,0 +1,107 @@
+import data from '../data.json';
+
+let usa = 0;
+let uk = 0;
+let cnd = 0;
+let aus = 0;
+let jpn = 0;
+let egpt = 0;
+let irlnd = 0;
+let skr = 0;
+let swdn =0;
+let frnc = 0
+
+data.data.map(elem => {
+    if(elem.country === 'USA') {
+        usa++
+    } else if(elem.country === 'UK') {
+        uk++
+    } else if(elem.country === 'Australia') {
+        aus++
+    } else if(elem.country === 'Japan') {
+        jpn++
+    } else if(elem.country === 'Egypt') {
+        egpt++
+    } else if(elem.country === 'Canada') {
+        cnd++
+    } else if(elem.country === 'Ireland') {
+        irlnd++
+    } else if(elem.country === 'South Korea') {
+        skr++
+    } else if(elem.country === 'Sweden') {
+        swdn++
+    } else if(elem.country === 'France') {
+        frnc++
+    }
+    return {usa, uk, aus, jpn, egpt, cnd, irlnd, skr, swdn, frnc};
+})
+
+export const markers = {
+    type: 'FeatureCollection',
+    features: [
+      {
+        coordinates: [-95.6650, 37.6000],
+        text: 'USA',
+        value: usa
+      },
+      {
+        coordinates: [-3.4433, 55.3618],
+        text: 'UK',
+        value: uk
+      },
+      {
+        coordinates: [133.2813, -26.4391],
+        text: 'Australia',
+        value: aus
+      },
+      {
+        coordinates: [-96.0000, 56.0000],
+        text: 'Canada',
+        value: cnd
+      },
+      {
+        coordinates: [139.745438, 35.658581],
+        text: 'Japan',
+        value: jpn
+      },
+      {
+        coordinates: [26.8358, 30.7957],
+        text: 'Egypt',
+        value: egpt
+      },
+      {
+        coordinates: [-8.24389, 53.41291],
+        text: 'Ireland',
+        value: irlnd
+      },
+      {
+        coordinates: [127.024612, 37.532600],
+        text: 'South Korea',
+        value: skr
+      },
+      {
+        coordinates: [18.063240, 59.334591],
+        text: 'Sweden',
+        value: swdn
+      },
+      {
+        coordinates: [1.7191, 46.7111],
+        text: 'France',
+        value: frnc
+      }
+    ].map(data => {
+        return {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: data.coordinates
+            },
+            properties: {
+                text: data.text,
+                value: data.value,
+                tooltip: `<b>${ data.text }</b>\n${ data.value }`
+            }
+        };
+    })
+};
+
--- src/components/map.js
+++ src/components/map.js
@@ -1,18 +1,65 @@
-import React, { Component } from 'react';
-import { Map, CircleMarker, TileLayer } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
+import React from 'react';
+import '../App.css';
+import VectorMap, {
+  Label,
+  Layer,
+  Legend,
+  Source,
+  Tooltip
+} from 'devextreme-react/vector-map';
 
-class MapChart extends Component {
-    render() {
-        return (
-            <Map
-                style={{ height: "480px", width: "100%" }}
-                zoom={1}
-                center={[-0.09, 51.505]}>
-                <TileLayer url="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            </Map>
-        );
-    }
-} 
+import * as mapsData from 'devextreme/dist/js/vectormap-data/world.js';
+import { markers } from './map-values';
 
-export default MapChart;
+const sizeGroups = [1, 5, 10, 40];
+
+const bounds = [-180, 85, 180, -60];
+
+export default function MapChart() {
+    
+    return (
+        <VectorMap id="vector-map" bounds={bounds}>
+            <Layer
+                dataSource={mapsData.world}
+                hoverEnabled={false}>
+            </Layer>
+            <Layer
+                dataSource={markers}
+                name="bubbles"
+                elementType="bubble"
+                dataField="value"
+                minSize={20}
+                maxSize={40}
+                sizeGroups={sizeGroups}
+                opacity="0.7">
+                <Label enabled={false}></Label>
+            </Layer>
+            <Tooltip 
+                enabled={true}
+                customizeTooltip={customizeTooltip}>
+            </Tooltip>
+            <Legend
+                markerShape="circle"
+                customizeItems={customizeItems}
+                customizeText={customizeText}>
+                <Source layer="bubbles" grouping="size"></Source>
+            </Legend>
+            <Tooltip enabled={true}
+                customizeTooltip={customizeTooltip} />
+        </VectorMap>
+    );
+}
+
+function customizeTooltip(arg) {
+  if (arg.layer.type === 'marker') {
+    return { text: arg.attribute('tooltip') };
+  }
+}
+
+function customizeText(arg) {
+  return ['< 5', '5 to 10', '> 10'][arg.index];
+}
+
+function customizeItems(items) {
+  return items.reverse();
+}
