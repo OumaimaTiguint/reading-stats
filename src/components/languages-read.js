d5799297a622db10fe32765b2aebe9f25de88c4a
OumaimaTiguint <oumaimatiguint@gmail.com>
2020-11-19 14:55:36 +0100

    data update

--- src/components/languages-read.js
+++ src/components/languages-read.js
@@ -6,6 +6,7 @@ import '../App.css';
 let english = 0;
 let french = 0;
 let arabic = 0;
+let japanese = 0;
 
 data.data.map(e => {
     if(e["Lang-read"]==="english") {
@@ -14,26 +15,30 @@ data.data.map(e => {
         return french ++;
     } else if(e["Lang-read"]==="arabic") {
         return arabic ++;
+    } else if(e["Lang-read"]==="japanese") {
+        return japanese ++;
     } 
-    return { english, french, arabic }
+    return { english, french, arabic, japanese }
 })
 
 const state = {
-    labels: ['English', 'French', 'Arabic'],
+    labels: ['English', 'French', 'Arabic', 'Japanese'],
     datasets: [
         {
             label: 'Languages Read',
             backgroundColor: [
                 '#B21F00',
                 '#C9DE00',
-                '#9a5863'
+                '#9a5863',
+                '#FFB6C1'
             ],
             hoverBackgroundColor: [
                 '#501800',
                 '#4B5000',
-                '#876974'
+                '#876974',
+                '#FF1493'
             ],
-            data: [english, french, arabic]
+            data: [english, french, arabic, japanese]
         }
     ]
 }
