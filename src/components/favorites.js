17bd350f4ebf68b47d75df386c01bc132fd19961
OumaimaTiguint <oumaimatiguint@gmail.com>
2021-02-24 21:19:45 +0100

    data update

--- src/components/favorites.js
+++ src/components/favorites.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import '../App.css';
 
-const favorites = []
+const favorites = [
+    { title: 'The Final Empire', img: 'the-final-empire.jpg' }
+]
 
 class Favorites extends React.Component {
     render() {
         return(
             <div className="favorites">
                 <h2>Favorites</h2>
-                {favorites.map((elem, i) => <img key={i} alt={elem.title} src={require('../favorites/' + elem.img)}/>)}
+                {favorites.map((elem, i) => <img key={i} alt={elem.title} src={require('../favorites/2021/' + elem.img)}/>)}
             </div>
             
         )
